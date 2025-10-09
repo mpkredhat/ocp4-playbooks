4a489cfe6e2711f4b6e1590ba904dca01ed31ab0
Mandar Kulkarni <mandakul@redhat.com>
2025-10-09 15:36:02 +0530

    Create restart-ovs-service.sh

---
+++ playbooks/roles/ocp-config/files/restart-ovs-service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#The interface name is passed as the first argument ($1)
+#The action is passed as the second argument ($2)
+
+    #Define the interface and service we care about
+    TARGET_INTERFACE="br-ex"
+    SERVICE_TO_RESTART="configure-ovs.service"
+
+    #Check if the event is for our target interface and the action is 'up' or 'dhcp4-change'
+    if [[ "$1" == "${TARGET_INTERFACE}" && ( "$2" == "up" || "$2" == "dhcp4-change" ) ]]; then
+        # Log the action for debugging purposes
+        logger "NetworkManager Dispatcher: IP assigned to ${TARGET_INTERFACE}, restarting ${SERVICE_TO_RESTART}."
+
+    # Restart the desired service
+    systemctl restart ${SERVICE_TO_RESTART}
+fi
